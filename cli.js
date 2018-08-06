8c53526c14ed300840efb4725e6aca5850d0c283
CeAnomaly <42145067+CeAnomaly@users.noreply.github.com>
2018-08-06 21:43:10 +0800

    Add files via upload

---
+++ cli.js
@@ -0,0 +1,76 @@
+#!/usr/bin/env node
+'use strict';
+const meow = require('meow');
+const updateNotifier = require('update-notifier');
+const help = require('./lib/help');
+const pkg = require('./package.json');
+const taskbook = require('.');
+
+const cli = meow(help, {
+  flags: {
+    help: {
+      type: 'boolean',
+      alias: 'h'
+    },
+    version: {
+      type: 'boolean',
+      alias: 'v'
+    },
+    archive: {
+      type: 'boolean',
+      alias: 'a'
+    },
+    restore: {
+      type: 'boolean',
+      alias: 'r'
+    },
+    task: {
+      type: 'boolean',
+      alias: 't'
+    },
+    note: {
+      type: 'boolean',
+      alias: 'n'
+    },
+    delete: {
+      type: 'boolean',
+      alias: 'd'
+    },
+    check: {
+      type: 'boolean',
+      alias: 'c'
+    },
+    star: {
+      type: 'boolean',
+      alias: 's'
+    },
+    timeline: {
+      type: 'boolean',
+      alias: 'i'
+    },
+    priority: {
+      type: 'boolean',
+      alias: 'p'
+    },
+    find: {
+      type: 'boolean',
+      alias: 'f'
+    },
+    list: {
+      type: 'boolean',
+      alias: 'l'
+    },
+    edit: {
+      type: 'boolean',
+      alias: 'e'
+    },
+    move: {
+      type: 'boolean',
+      alias: 'm'
+    }
+  }
+});
+
+updateNotifier({pkg}).notify();
+
+taskbook(cli.input, cli.flags);
---
+++ index.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+'use strict';
+const taskbook = require('./lib/taskbook');
+
+const taskbookCLI = (input, flags) => {
+  if (flags.archive) {
+    return taskbook.displayArchive();
+  }
+
+  if (flags.task) {
+    return taskbook.createTask(input);
+  }
+
+  if (flags.restore) {
+    return taskbook.restoreItems(input);
+  }
+
+  if (flags.note) {
+    return taskbook.createNote(input);
+  }
+
+  if (flags.delete) {
+    return taskbook.deleteItems(input);
+  }
+
+  if (flags.check) {
+    return taskbook.checkTasks(input);
+  }
+
+  if (flags.star) {
+    return taskbook.starItems(input);
+  }
+
+  if (flags.priority) {
+    return taskbook.updatePriority(input);
+  }
+
+  if (flags.timeline) {
+    taskbook.displayByDate();
+    return taskbook.displayStats();
+  }
+
+  if (flags.find) {
+    return taskbook.findItems(input);
+  }
+
+  if (flags.list) {
+    taskbook.listByAttributes(input);
+    return taskbook.displayStats();
+  }
+
+  if (flags.edit) {
+    return taskbook.editDescription(input);
+  }
+
+  if (flags.move) {
+    return taskbook.moveBoards(input);
+  }
+
+  taskbook.displayByBoard();
+  return taskbook.displayStats();
+};
+
+module.exports = taskbookCLI;
